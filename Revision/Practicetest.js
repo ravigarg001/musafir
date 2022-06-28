28493837e2fac67151cfb4ab54adee39abd779e0
Ravi Garg <gravi8588@gmail.com>
2022-06-28 16:09:32 +0530

    fixed Totallikes at wrong place

--- Revision/Practicetest.js
+++ Revision/Practicetest.js
@@ -244,8 +244,8 @@ alert(`Open console for Pattern`);
 
 let truthArray = [`Truth is such a sword that years of tightly held chains break away just by a touch of it in just one moment.`,`force is futile`,`And then he leave`,'Life is a falling leaf','In cycles move the life of ordinary man','Man default to his defaults','Be SuperNatural - Instinct not the Instructions'];
 let showTruth = () => {
-    let likeCount = () => {
-	let totalLikes = 0;
+    let totalLikes = 0;
+    let likeCount = () => 
         let like = 0;
         ++like;
         totalLikes += like;
